eaeaa071d9fb4e8456e83540266b93a28d7c13c3
B3AL <92798364+B3AL@users.noreply.github.com>
2021-11-17 09:07:11 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,13 @@
+// counterclockwise
+input.onButtonPressed(Button.A, function () {
+    // This is a speed of about 1sec./rotation
+    pins.servoWritePin(AnalogPin.P0, 110)
+})
+input.onButtonPressed(Button.AB, function () {
+    // This is a speed of about 1sec./rotation
+    pins.servoWritePin(AnalogPin.P0, 90)
+})
+// clockwise
+input.onButtonPressed(Button.B, function () {
+    pins.servoWritePin(AnalogPin.P0, 65)
+})
